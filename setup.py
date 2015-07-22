e3714b9b9d26592f6d7da9c4554f39e329419a16
Thomas Nipen <tnipen@gmail.com>
2015-07-22 10:42:19 -0700

    Adds scipy dependency to setup

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['numpy', 'matplotlib', 'scipy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
