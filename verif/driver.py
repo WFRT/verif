f5d1c9dcdd81ecc3f8952a235d319c7e071f7e2a
Thomas Nipen <tnipen@gmail.com>
2016-12-29 15:42:17 +0100

    Change -x none to -x no in documentation

--- verif/driver.py
+++ verif/driver.py
@@ -569,7 +569,7 @@ def show_description(data=None):
    print format_argument("-r thresholds", "Compute scores using these thresholds (only used by some metrics).")
    print format_argument("-q quantiles", "Compute scores using these quantiles (only used by some metrics).")
    print format_argument("-t times", "A vector of unix timestamps.")
-   print format_argument("-x dim", "Plot this dimension on the x-axis: date, leadtime, year, month, week, location, locationid, elev, lat, lon, threshold, or none. Not supported by all metrics. If not specified, then a default is used based on the metric. 'none' collapses all dimensions and computes one value.")
+   print format_argument("-x dim", "Plot this dimension on the x-axis: date, leadtime, year, month, week, location, locationid, elev, lat, lon, threshold, or no. Not supported by all metrics. If not specified, then a default is used based on the metric. 'no' collapses all dimensions and computes one value.")
 
    # Data manipulation
    print verif.util.green("  Data manipulation:")
