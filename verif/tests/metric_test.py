ea753f12dd3af71b3d4d57b33dfbc5cc5948dd1f
Thomas Nipen <tnipen@gmail.com>
2016-12-31 17:01:28 +0100

    Added more metric tests

--- verif/tests/metric_test.py
+++ verif/tests/metric_test.py
@@ -24,6 +24,38 @@ class MyTest(unittest.TestCase):
       mae = verif.metric.Mae()
       self.assertEqual("MAE", mae.name())
 
+class TestRmse(unittest.TestCase):
+   def _get(self):
+      inputs = [verif.input.get_input(file) for file in ["examples/raw.txt", "examples/kf.txt"]]
+      data = verif.data.Data(inputs=inputs)
+      return data
+
+   def test_basic(self):
+      data = self._get()
+      metrics = [verif.metric.Rmse(),
+                 verif.metric.Mae(),
+                 verif.metric.Bias(),
+                 verif.metric.Corr(),
+                 verif.metric.Mbias(),
+                 ]
+      obs = [[0,1.5,2],[0]] # Case1, Case 2, ...
+      fcst = [[3.1,1.1,-2.1],[1]]
+      expected = [[2.976575213,1], # Rmse
+                  [2.5333333333,1], # Mae
+                  [-0.466666666666,1], # Bias
+                  [-0.915724295,np.nan], # Corr
+                  [0.6,np.nan], # Mbias
+                  ]
+
+      for m in range(0,len(metrics)):
+         metric = metrics[m]
+         for i in range(0, len(obs)):
+            value = metric.compute_from_obs_fcst(np.array(obs[i]), np.array(fcst[i]))
+            if np.isnan(value):
+               self.assertTrue(np.isnan(expected[m][i]))
+            else:
+               self.assertAlmostEqual(expected[m][i], value)
+
 
 if __name__ == '__main__':
    unittest.main()
