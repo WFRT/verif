228c21af351c31798c35e3b370afee68b5f5531f
Thomas Nipen <tnipen@gmail.com>
2025-11-10 22:12:26 +0100

    Interpolate ensembles as well

--- scripts/time_interpolation.py
+++ scripts/time_interpolation.py
@@ -36,6 +36,10 @@ def main():
         leadtimes = file.variables["leadtime"][:]
         num_leadtimes = len(leadtimes)
 
+        has_ens = "ensemble" in file.variables
+        if has_ens:
+            ensemble = file.variables["ensemble"][:]
+
         if frequency >= num_leadtimes:
             raise ValueError(f"frequency must be < {num_leadtimes}")
 
@@ -45,6 +49,7 @@ def main():
             ends = range(frequency, num_leadtimes+1, frequency)
             for end in ends:
                 start = end - frequency
+
                 fcst[:, (start+1):end, :] = np.nan
                 fcst_start = fcst[:, start, :]
                 fcst_end = fcst[:, end, :]
@@ -54,8 +59,19 @@ def main():
                     fcst[:, start + i, :] = w0 * fcst_start + w1 * fcst_end
                     # print(start, end, i, w0, w1)
 
+                if has_ens:
+                    ensemble[:, (start+1):end, :, :] = np.nan
+                    fcst_start = ensemble[:, start, :, :]
+                    fcst_end = ensemble[:, end, :]
+                    for i in range(1, frequency):
+                        w1 = i / frequency
+                        w0 = 1 - w1
+                        ensemble[:, start + i, :, :] = w0 * fcst_start + w1 * fcst_end
+
             # Trim the last leadtimes that cannot be interpolated
             fcst[:, ends[-1]+1:, :] = np.nan
+            if has_ens:
+                ensemble[:, ends[-1]+1:, :, :] = np.nan
 
         elif args.interpolator == "spline":
             y = fcst[:, 0::frequency, :]
@@ -68,9 +84,74 @@ def main():
             for i, index in enumerate(xx):
                 fcst[:, int(index), :] = temp[:, i, :]
 
+            if has_ens:
+                y = ensemble[:, 0::frequency, :, :]
+                x = leadtimes[0::frequency]
 
+                interpolator = scipy.interpolate.CubicSpline(x, y, axis=1)
+                xx = set([i for i in leadtimes]) - set([i for i in x])
+                xx = np.array(list(xx))
+                temp = interpolator(xx)
+                for i, index in enumerate(xx):
+                    ensemble[:, int(index), :, :] = temp[:, i, :, :]
 
         file.variables["fcst"][:] = fcst
+        if has_ens:
+            file.variables["ensemble"][:] = ensemble
+
+        # These variables are no longer valid, since we cannot derive them for interpolated
+        # timeseries
+        invalid_variables = ["x", "cdf", "pdf", "pit"]
+        for variable in invalid_variables:
+            if variable in file.variables:
+                file.variables[variable] = np.nan
+
+        # Compute CRPS
+        if has_ens:
+            if not "crps" in file.variables:
+                file.createVariable("crps", "f4", ("time", "leadtime", "location"))
+
+            num_members = ensemble.shape[-1]
+            crps = compute_crps(ensemble, file.variables["obs"][:], num_members)
+
+            num_valid_members = np.sum(~np.isnan(ensemble), axis=3)
+            crps[num_valid_members != num_members] = np.nan
+
+            file.variables["crps"][:] = crps
+
+
+def compute_crps(preds, targets, num_members, fair=True) -> np.ndarray:
+    """Continuous Ranked Probability Score (CRPS).
+
+    Taken from bris-inference
+
+    Args:
+        preds: numpy.ndarray
+            Predictions, shape (time, leadtime, location, ens_size)
+        targets: numpy.ndarray
+            Targets, shape (time, leadtime, location)
+        fair: bool
+            Defaults to true
+
+    Returns:
+        crps: numpy.ndarray
+            Shape (time, leadtime, location)
+    """
+
+    coef = (
+        -1.0 / (num_members * (num_members - 1))
+        if fair
+        else -1.0 / (num_members**2)
+    )
+
+    mae = np.mean(np.abs(targets[..., None] - preds), axis=-1)
+
+    # var = np.abs(preds[..., None] - preds[..., None, :])
+    var = np.zeros(preds.shape[:-1])
+    for i in range(num_members):  # loop version to reduce memory usage
+        var += np.sum(np.abs(preds[..., i, None] - preds[..., i + 1 :]), axis=-1)
+    var *= coef
+    return mae + var
 
 
 if __name__ == "__main__":
